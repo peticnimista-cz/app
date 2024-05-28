06b0cd012145235a0f9424c31d1eebb6000af069
Miloslav Turyna <milan@turyna.eu>
2024-05-28 07:48:33 +0200

    basic color palette

---
+++ www/js/tailwind.config.js
@@ -0,0 +1,14 @@
+module.exports = {
+    theme: {
+        colors: {
+            transparent: 'transparent',
+            current: 'currentColor',
+            'brand': '#1B264F',
+            "brand-secondary": "#EAF7FF",
+            'brand-light': '#3f3cbb',
+            'secondary': '#797979',
+            'secondary-2': '#403E3E',
+            'user': '#FF7846',
+        },
+    },
+}
