19dd71592f490fe895f24713a23f8e82a67fe8f6
Miloslav Turyna <milan@turyna.eu>
2024-05-30 10:57:07 +0200

    templates, colors

--- www/js/tailwind.config.js
+++ www/js/tailwind.config.js
@@ -6,11 +6,21 @@ const TW_settings = {
             colors: {
                 transparent: 'transparent',
                 current: 'currentColor',
+
+                "petition-success": "#138900",
+                "petition-success-light": "#EBFFEA",
+                "petition-waiting": "#897300",
+                "petition-waiting-light": "#FFFCEA",
+                "petition-decline": "#890000",
+                "petition-decline-light": "#FFEAEA",
+
                 'brand': '#1B264F',
                 "brand-light": "#EAF7FF",
                 'brand-secondary': '#005689',
+
                 'secondary': '#797979',
                 'secondary-2': '#403E3E',
+
                 'user': '#FF7846',
             },
         }
